e598f0c8a980cf110dcd97cff7c58e0b9fbf0fce
kyupid <iamscvv@gmail.com>
2021-09-11 01:01:27 +0900

    Basic extractor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,35 @@ import org.jsoup.nodes.Document;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i < 96 + 1; i++) {
-            String date = new DateGenerator().setRange(i);
-            String url = "https://velog.io/@kyukim/" + date;
-            Document doc = Jsoup.connect(url).get();
+        Date currentDate = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String formattedDate = format.format(currentDate);
 
-            OptionsBuilder optionsBuilder = OptionsBuilder.anOptions();
-            Options options = optionsBuilder
-                    .withHeadingStyle(HeadingStyle.ATX)
-                    .withBulletListMaker("-")
-                    .withCodeBlockStyle(CodeBlockStyle.FENCED)
-                    .build();
+        String url = "https://velog.io/@kyukim/" + formattedDate;
+        Document doc = Jsoup.connect(url).get();
 
-            CopyDown converter = new CopyDown(options);
-            String myHtml = doc.select(".sc-bbmXgH").toString();
-            String markdown = converter.convert(myHtml);
+        OptionsBuilder optionsBuilder = OptionsBuilder.anOptions();
+        Options options = optionsBuilder
+                .withHeadingStyle(HeadingStyle.ATX)
+                .withBulletListMaker("-")
+                .withCodeBlockStyle(CodeBlockStyle.FENCED)
+                .build();
 
-            String pathName = date + ".md";
-            File file = new File(pathName);
-            FileWriter fw = new FileWriter(file);
+        CopyDown converter = new CopyDown(options);
+        String myHtml = doc.select(".sc-bbmXgH").toString();
+        String markdown = converter.convert(myHtml);
 
-            fw.write(markdown);
-            fw.flush();
-            fw.close();
-        }
+        String pathName = formattedDate + ".md";
+        File file = new File(pathName);
+        FileWriter fw = new FileWriter(file);
 
+        fw.write(markdown);
+        fw.flush();
+        fw.close();
     }
 }
